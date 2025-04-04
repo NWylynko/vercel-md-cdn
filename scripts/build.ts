81f5ac3f3b494744e1aeca52788e749ceb3dd286
Nick Wylynko <nickwylynko@clerk.dev>
2025-04-05 05:31:12 +0800

    first attempt

---
+++ scripts/build.ts
@@ -0,0 +1,19 @@
+import fs from "fs/promises";
+import path from "path";
+
+const main = async () => {
+  const filePath = path.join(__dirname, "../docs/index.md");
+  const distPath = path.join(__dirname, "../output/index.md");
+
+  const index = await fs.readFile(filePath);
+
+  const output = `# Date: ${new Date().toISOString()}
+${index}
+`;
+
+  await fs.mkdir(path.dirname(distPath), { recursive: true });
+
+  await fs.writeFile(distPath, output);
+};
+
+main();
